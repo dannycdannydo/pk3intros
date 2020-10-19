9a1f31a6920e64992caf1ddd5a77c31903c3a230
dannycdannydo <51877703+dannycdannydo@users.noreply.github.com>
2020-10-19 22:46:38 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,12 +1,10 @@
 import fetch_mail
 import insert_to_db
 
-
 def process():
     email_data = fetch_mail.get_email_data()
-    print(email_data)
     for i in range(len(email_data)):
-        insert_to_db.insert_to_db(email_data)
+        insert_to_db.insert_to_db(email_data[i])
 
 
 process()
