55a9409cb3e20a4969a8eaf935975aecc18be62b
YunYun <daniel.campbell90@gmali.com>
2020-10-20 21:26:29 +0100

    update requirements.txt

--- app.py
+++ app.py
@@ -7,16 +7,18 @@ import os
 from flask import Flask
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
-
-
+@app.route('/')
 def process():
     email_data = fetch_mail.get_email_data()
     for i in range(len(email_data)):
         insert_to_db.insert_to_db(email_data[i])
     return "Hello from Python!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
+
 
 schedule.every(5).seconds.do(process)
 
