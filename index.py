22b019bcf50f8d6ca1d6f27244fdb25e2728659b
YunYun <daniel.campbell90@gmali.com>
2020-10-20 20:24:48 +0100

    update requirements.txt

--- index.py
+++ index.py
@@ -14,3 +14,9 @@ schedule.every(5).seconds.do(process)
 while True:
     schedule.run_pending()
     time.sleep(1)
+
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
