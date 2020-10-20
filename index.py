8a20ebda5f0a1ebcc32927c8095193e87bedd137
YunYun <daniel.campbell90@gmali.com>
2020-10-20 20:47:53 +0100

    update requirements.txt

--- index.py
+++ index.py
@@ -3,20 +3,23 @@ import insert_to_db
 import schedule
 import time
 
-def process():
-    email_data = fetch_mail.get_email_data()
-    for i in range(len(email_data)):
-        insert_to_db.insert_to_db(email_data[i])
+import os
+from flask import Flask
+app = Flask(__name__)
 
-schedule.every(5).seconds.do(process)
+@app.route("/")
+def hello():
+    def process():
+        email_data = fetch_mail.get_email_data()
+        for i in range(len(email_data)):
+            insert_to_db.insert_to_db(email_data[i])
 
+    schedule.every(5).seconds.do(process)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
-
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
