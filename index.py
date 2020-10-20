126906b949ddda007e0c6575a0f13238852ff933
YunYun <daniel.campbell90@gmali.com>
2020-10-20 20:51:13 +0100

    update requirements.txt

--- index.py
+++ index.py
@@ -8,17 +8,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    def process():
-        email_data = fetch_mail.get_email_data()
-        for i in range(len(email_data)):
-            insert_to_db.insert_to_db(email_data[i])
+def process():
+    email_data = fetch_mail.get_email_data()
+    for i in range(len(email_data)):
+        insert_to_db.insert_to_db(email_data[i])
 
-    schedule.every(5).seconds.do(process)
+schedule.every(5).seconds.do(process)
 
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
