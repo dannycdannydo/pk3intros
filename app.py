1ee17ff65ead25ac2deff8c3dc94d918d62253fc
YunYun <daniel.campbell90@gmali.com>
2020-10-20 21:14:24 +0100

    update requirements.txt

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ import os
 from flask import Flask
 app = Flask(__name__)
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
 @app.route("/")
 def process():
     email_data = fetch_mail.get_email_data()
@@ -20,6 +24,3 @@ while True:
     schedule.run_pending()
     time.sleep(1)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
