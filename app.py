e8cbb1382e004c6dbe76a5f01d4bd3763bc4672c
YunYun <daniel.campbell90@gmali.com>
2020-10-20 21:17:43 +0100

    update requirements.txt

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
 
-@app.route("/")
+
 def process():
     email_data = fetch_mail.get_email_data()
     for i in range(len(email_data)):
